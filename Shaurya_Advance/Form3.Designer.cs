66db4287b608c456b17498659627e2886128d053
Snehal <pachundkars6@gmail.com>
2022-07-11 17:16:39 +0530

    Updated On Form 3 Again

--- Shaurya_Advance/Form3.Designer.cs
+++ Shaurya_Advance/Form3.Designer.cs
@@ -48,7 +48,7 @@ namespace Shaurya_Advance
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(143, 31);
+            this.label1.Location = new System.Drawing.Point(89, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 0;
@@ -56,14 +56,14 @@ namespace Shaurya_Advance
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(208, 24);
+            this.txtId.Location = new System.Drawing.Point(154, 25);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(170, 20);
             this.txtId.TabIndex = 1;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(208, 62);
+            this.txtName.Location = new System.Drawing.Point(154, 63);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(170, 20);
             this.txtName.TabIndex = 3;
@@ -71,7 +71,7 @@ namespace Shaurya_Advance
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(127, 69);
+            this.label2.Location = new System.Drawing.Point(73, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 2;
@@ -79,7 +79,7 @@ namespace Shaurya_Advance
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(208, 88);
+            this.txtPrice.Location = new System.Drawing.Point(154, 89);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(170, 20);
             this.txtPrice.TabIndex = 5;
@@ -87,7 +87,7 @@ namespace Shaurya_Advance
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(127, 95);
+            this.label3.Location = new System.Drawing.Point(73, 96);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 13);
             this.label3.TabIndex = 4;
@@ -95,9 +95,9 @@ namespace Shaurya_Advance
             // 
             // btnBinaryRead
             // 
-            this.btnBinaryRead.Location = new System.Drawing.Point(268, 143);
+            this.btnBinaryRead.Location = new System.Drawing.Point(214, 147);
             this.btnBinaryRead.Name = "btnBinaryRead";
-            this.btnBinaryRead.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryRead.Size = new System.Drawing.Size(75, 20);
             this.btnBinaryRead.TabIndex = 6;
             this.btnBinaryRead.Text = "Binary Read";
             this.btnBinaryRead.UseVisualStyleBackColor = true;
@@ -105,9 +105,9 @@ namespace Shaurya_Advance
             // 
             // btnBinaryWrite
             // 
-            this.btnBinaryWrite.Location = new System.Drawing.Point(174, 143);
+            this.btnBinaryWrite.Location = new System.Drawing.Point(120, 147);
             this.btnBinaryWrite.Name = "btnBinaryWrite";
-            this.btnBinaryWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryWrite.Size = new System.Drawing.Size(75, 20);
             this.btnBinaryWrite.TabIndex = 7;
             this.btnBinaryWrite.Text = "Binary Write";
             this.btnBinaryWrite.UseVisualStyleBackColor = true;
@@ -115,63 +115,69 @@ namespace Shaurya_Advance
             // 
             // btnXmlWrite
             // 
-            this.btnXmlWrite.Location = new System.Drawing.Point(174, 172);
+            this.btnXmlWrite.Location = new System.Drawing.Point(120, 176);
             this.btnXmlWrite.Name = "btnXmlWrite";
-            this.btnXmlWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlWrite.Size = new System.Drawing.Size(75, 20);
             this.btnXmlWrite.TabIndex = 8;
             this.btnXmlWrite.Text = "Xml Write";
             this.btnXmlWrite.UseVisualStyleBackColor = true;
+            this.btnXmlWrite.Click += new System.EventHandler(this.btnXmlWrite_Click);
             // 
             // btnXmlRead
             // 
-            this.btnXmlRead.Location = new System.Drawing.Point(268, 172);
+            this.btnXmlRead.Location = new System.Drawing.Point(214, 176);
             this.btnXmlRead.Name = "btnXmlRead";
-            this.btnXmlRead.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlRead.Size = new System.Drawing.Size(75, 20);
             this.btnXmlRead.TabIndex = 9;
             this.btnXmlRead.Text = "Xml Read";
             this.btnXmlRead.UseVisualStyleBackColor = true;
+            this.btnXmlRead.Click += new System.EventHandler(this.btnXmlRead_Click);
             // 
             // btnSOAPWrite
             // 
-            this.btnSOAPWrite.Location = new System.Drawing.Point(174, 201);
+            this.btnSOAPWrite.Location = new System.Drawing.Point(120, 205);
             this.btnSOAPWrite.Name = "btnSOAPWrite";
-            this.btnSOAPWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnSOAPWrite.Size = new System.Drawing.Size(75, 20);
             this.btnSOAPWrite.TabIndex = 10;
             this.btnSOAPWrite.Text = "SOAP Write";
             this.btnSOAPWrite.UseVisualStyleBackColor = true;
+            this.btnSOAPWrite.Click += new System.EventHandler(this.btnSOAPWrite_Click);
             // 
             // btnSOAPRead
             // 
-            this.btnSOAPRead.Location = new System.Drawing.Point(268, 201);
+            this.btnSOAPRead.Location = new System.Drawing.Point(214, 205);
             this.btnSOAPRead.Name = "btnSOAPRead";
-            this.btnSOAPRead.Size = new System.Drawing.Size(75, 23);
+            this.btnSOAPRead.Size = new System.Drawing.Size(75, 20);
             this.btnSOAPRead.TabIndex = 11;
             this.btnSOAPRead.Text = "SOAP Read";
             this.btnSOAPRead.UseVisualStyleBackColor = true;
+            this.btnSOAPRead.Click += new System.EventHandler(this.btnSOAPRead_Click);
             // 
             // btnJSONWrite
             // 
-            this.btnJSONWrite.Location = new System.Drawing.Point(174, 230);
+            this.btnJSONWrite.Location = new System.Drawing.Point(120, 234);
             this.btnJSONWrite.Name = "btnJSONWrite";
-            this.btnJSONWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnJSONWrite.Size = new System.Drawing.Size(75, 20);
             this.btnJSONWrite.TabIndex = 12;
             this.btnJSONWrite.Text = "JSON Write";
             this.btnJSONWrite.UseVisualStyleBackColor = true;
+            this.btnJSONWrite.Click += new System.EventHandler(this.btnJSONWrite_Click);
             // 
             // btnJSONRead
             // 
-            this.btnJSONRead.Location = new System.Drawing.Point(268, 230);
+            this.btnJSONRead.Location = new System.Drawing.Point(214, 234);
             this.btnJSONRead.Name = "btnJSONRead";
-            this.btnJSONRead.Size = new System.Drawing.Size(75, 23);
+            this.btnJSONRead.Size = new System.Drawing.Size(75, 20);
             this.btnJSONRead.TabIndex = 13;
             this.btnJSONRead.Text = "JSON Read";
             this.btnJSONRead.UseVisualStyleBackColor = true;
+            this.btnJSONRead.Click += new System.EventHandler(this.btnJSONRead_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(403, 273);
             this.Controls.Add(this.btnJSONRead);
             this.Controls.Add(this.btnJSONWrite);
             this.Controls.Add(this.btnSOAPRead);
--- Shaurya_Advance/Form3.cs
+++ Shaurya_Advance/Form3.cs
@@ -6,9 +6,12 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Text.Json;
 
 namespace Shaurya_Advance
 {
@@ -64,6 +67,128 @@ namespace Shaurya_Advance
                 MessageBox.Show(ex.Message);
             }
 
+        }
+
+        private void btnXmlWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product p = new Product();
+                p.Id = Convert.ToInt32(txtId.Text);
+                p.Name = txtName.Text;
+                p.Price = Convert.ToInt32(txtPrice.Text);
+                FileStream fs = new FileStream(@"E:\ProductXml", FileMode.Create, FileAccess.Write);
+                XmlSerializer xs = new XmlSerializer(typeof(Product));
+                xs.Serialize(fs, p);
+                MessageBox.Show("Xml File Created");
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnXmlRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product p = new Product();
+                FileStream fs = new FileStream(@"E:\ProductXml", FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(Product));
+                p = (Product)xs.Deserialize(fs);
+                txtId.Text = p.Id.ToString();
+                txtName.Text = p.Name;
+                txtPrice.Text = p.Price.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSOAPWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product p = new Product();
+                p.Id = Convert.ToInt32(txtId.Text);
+                p.Name = txtName.Text;
+                p.Price = Convert.ToInt32(txtPrice.Text);
+                FileStream fs = new FileStream(@"E:\ProductSoap", FileMode.Create, FileAccess.Write);
+                SoapFormatter sf = new SoapFormatter();
+                sf.Serialize(fs, p);
+                MessageBox.Show("Soap File Created");
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnSOAPRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product p = new Product();
+                FileStream fs = new FileStream(@"E:\ProductSoap", FileMode.Open, FileAccess.Read);
+                SoapFormatter sf = new SoapFormatter();
+                p = (Product)sf.Deserialize(fs);
+                txtId.Text = p.Id.ToString();
+                txtName.Text = p.Name;
+                txtPrice.Text = p.Price.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJSONWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product p = new Product();
+                p.Id = Convert.ToInt32(txtId.Text);
+                p.Name = txtName.Text;
+                p.Price = Convert.ToInt32(txtPrice.Text);
+                FileStream fs = new FileStream(@"E:\ProductJson", FileMode.Create, FileAccess.Write);
+                JsonSerializer.Serialize(fs, p);
+                MessageBox.Show("Json File Created");
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnJSONRead_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Product p = new Product();
+                FileStream fs = new FileStream(@"E:\ProductJson", FileMode.Open, FileAccess.Read);
+                SoapFormatter sf = new SoapFormatter();
+                p = JsonSerializer.Deserialize<Product>(fs);
+                txtId.Text = p.Id.ToString();
+                txtName.Text = p.Name;
+                txtPrice.Text = p.Price.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
